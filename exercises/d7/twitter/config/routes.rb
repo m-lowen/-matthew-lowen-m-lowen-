9094d90cbec0b2f69b1d7af590f5a30c547be62d
m-lowen <mcl7zz@virginia.edu>
2015-06-09 22:36:17 +0200

    d7 final push

--- exercises/d7/twitter/config/routes.rb
+++ exercises/d7/twitter/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :tweets
   resources :users
+
+  #gets '/home' => 'controller_name#action'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
